62277a2b986c2fbc9496fe537e1010656eb0d9ad
Samuel Sousa <ssousa@inoa.com.br>
2023-05-24 07:28:57 -0300

    gitignore update

--- server/database/database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var dotenv_1 = require("dotenv");
-dotenv_1["default"].config();
-var pg_1 = require("pg");
-var pool = new pg_1["default"].Pool();
-var res = await pool.query('SELECT NOW()');
-await pool.end();
-console.log(res.rows[0].now);
